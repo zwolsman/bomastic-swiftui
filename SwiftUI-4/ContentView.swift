e17ceb6e73e35c320100e0eaab42c357b2335d0b
Marvin <marvin.zwolsman@gmail.com>
2020-03-21 17:53:40 +0100

    Initial Commit

---
+++ SwiftUI-4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-4
+//
+//  Created by Marvin Zwolsman on 21/03/2020.
+//  Copyright © 2020 Marvin Zwolsman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
